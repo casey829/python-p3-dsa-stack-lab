48ff6481d2f4f74aec225577511569aa54f73b79
casey <caseymwangi31@gmail.com>
2024-06-13 15:11:36 +0300

    understand dsa

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,38 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        if items is None:
+            items = []
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if self.size() < self.limit:
+            self.items.append(item)
 
     def pop(self):
-        pass
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            return None
 
     def peek(self):
-        pass
+        if not self.isEmpty():
+            return self.items- 1
+        else:
+            return None
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) == self.limit
 
     def search(self, target):
-        pass
+        if target in self.items:
+            return self.items[::-1].index(target)
+        else:
+            return -1
